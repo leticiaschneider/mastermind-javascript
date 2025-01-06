15ffe6830cce1987d39f42e6e7e2d314a0efe033
Leticia <leticia.schneider@gmail.com>
2025-01-06 09:27:16 -0300

    instructions for how to play

--- script.js
+++ script.js
@@ -63,3 +63,24 @@ for (let i = 0; i < repetitions; i++) {
         boardSection.appendChild(hr);
     }
 }
+
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+const modal = document.getElementById("howToPlayModal");
+
+// Open modal
+openModal.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+// Close modal
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close modal when clicking outside of it
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
